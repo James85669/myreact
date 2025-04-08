87db0c8895546ff1053f2f4b3e4ce802b3d2bb4d
James85669 <james85669@gmai.com>
2025-04-08 17:00:53 +0800

    App0408

---
+++ src/App0408.jsx
@@ -0,0 +1,8 @@
+function App(){
+    return ( 
+    <>
+    <h1>Hi, React</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos voluptas corrupti distinctio ducimus aliquid ipsum. Voluptate, quas fugiat quibusdam dolore at, quidem exercitationem officia minima voluptates delectus perspiciatis rem? </p>
+    </>
+) }
+export default App;
